43c2a3955ec7b5bc474e8a1d0453df3f1e48e4bd
markcyril28 <mercadomarkcyril28@gmail.com>
2026-01-04 09:40:13 +0800

    Add setup.py for microspore_training package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="microspore_training",
+    version="1.0.0",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    author="Microspore Phenotyping Team",
+    description="Training modules for microspore phenotyping YOLO models",
+)
